317883a6942e69c91456bb5f226efc8ecfb81156
Avishay Hadar <avishih6@gmail.com>
2018-07-24 18:52:23 +0300

    first commit

---
+++ Item.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace SerializationExercise {
+    /// <summary>
+    /// This container is used for storing object data
+    /// </summary>
+    public class Item {
+        /// <summary>
+        /// The HashCode of the object
+        /// </summary>
+        public int HashCode;
+
+        /// <summary>
+        /// The type of the object
+        /// </summary>
+        public string Type;
+
+        /// <summary>
+        /// The value of the object if it's primitive/builtin 'base' type from namespace System
+        /// </summary>
+        public string Value;
+
+        /// <summary>
+        /// For objects that are not primitive/builtin, this contains Key:(field name) of the field, Value:(HashCode) of the field 
+        /// </summary>
+        public Dictionary<string, int> Fields = new Dictionary<string, int> ();
+    }
+
+    public class SerializationHelper {
+        private Dictionary<int, Item> cache = new Dictionary<int, Item> ();
+        private BindingFlags bindingFlags = BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance;
+
+        public Dictionary<int, Item> SerializeFields<T> (T t) {
+            CreateCache (t);
+            return cache;
+        }
+
+        private void CreateCache<T> (T t) {
+            var hashCode = t.GetHashCode ();
+
+            Item item;
+            if (cache.TryGetValue (hashCode, out item))
+                return;
+            else
+                item = new Item ();
+
+            item.Type = t.GetType ().FullName;
+            item.HashCode = hashCode;
+
+            cache.Add (hashCode, item);
+
+            if (t.GetType ().Namespace == "System") {
+                item.Value = t.ToString ();
+            } else {
+                var fields = t.GetType ().GetFields (bindingFlags);
+                foreach (var field in fields) {                    
+                    CreateCache (field.GetValue (t));
+                    item.Fields.Add (field.Name, field.GetValue (t).GetHashCode ());
+                }
+            }
+        }
+    }
+
+    public class DeserializationHelper {
+        private Dictionary<int, object> objectCache = new Dictionary<int, object> ();
+        private Dictionary<int, Item> itemCache;
+        private BindingFlags bindingFlags = BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance;
+
+        public DeserializationHelper (Dictionary<int, Item> itemCache) {
+            this.itemCache = itemCache;
+        }
+
+        public object DeserializeFromCache (int rootHashCode) {
+            var itemRoot = itemCache[rootHashCode];
+            return Generate (itemRoot);
+        }
+
+        private object Generate (Item item) {
+            if (objectCache.ContainsKey (item.HashCode)) {
+                return objectCache[item.HashCode];
+            }
+
+            // System.String does not have default constructor and is initiated as a special case.
+            // Obviously this would have to be extended for other types that dont have default constructors.
+            // This is the reason why deserialization by default requires default constructors
+            var instance = item.Type == "System.String" ? Activator.CreateInstance (Type.GetType (item.Type),
+                new object[] { item.Value.ToCharArray () }) : Activator.CreateInstance (Type.GetType (item.Type));
+
+            if (!objectCache.ContainsKey (item.HashCode))
+                objectCache.Add (item.HashCode, instance);
+
+            var fields = instance.GetType ().GetFields (bindingFlags);
+
+            if (Type.GetType (item.Type).Namespace == "System" && item.Type != "System.String") {
+                var value = Convert.ChangeType (item.Value, Type.GetType (item.Type));
+                fields.First ().SetValue (instance, value);
+            } else {
+                int hash;
+                foreach (var field in fields) {                    
+                    if(item.Fields.TryGetValue(field.Name, out hash)){                                              
+                        if (objectCache.ContainsKey (hash)) {
+                            field.SetValue (instance, objectCache[hash]);
+                        } else {
+                            var val = Generate (itemCache[hash]);
+                            field.SetValue (instance, val);
+                        }
+                    }                  
+                }
+            }
+
+            return instance;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Xml;
+
+namespace SerializationExercise {
+    class Person {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        // public static DateTime DateOfBirth { get; set;}
+        public Person Spause { get; set; }
+    }
+
+    public static class Serializer {
+        private static void WriteToXml (XmlDocument doc, XmlElement first, Item item) {
+            XmlElement element = doc.CreateElement ("", "item", "");
+            XmlElement elementHash = doc.CreateElement ("", "hashCode", "");
+            XmlText textHash = doc.CreateTextNode (item.HashCode.ToString ());
+            XmlElement elementType = doc.CreateElement ("", "type", "");
+            XmlText textType = doc.CreateTextNode (item.Type);
+            XmlElement elementValue = doc.CreateElement ("", "value", "");
+            XmlText textValue = doc.CreateTextNode (item.Value);
+
+            XmlElement elementItems = doc.CreateElement ("", "items", "");
+
+            foreach (var subItem in item.Fields) {
+                XmlElement sub = doc.CreateElement ("", "item", "");
+                XmlElement name = doc.CreateElement ("", "name", "");
+                XmlText textName = doc.CreateTextNode (subItem.Key);
+                XmlElement val = doc.CreateElement ("", "value", "");
+                XmlText textVal = doc.CreateTextNode (subItem.Value.ToString ());
+                name.AppendChild (textName);
+                val.AppendChild (textVal);
+                sub.AppendChild (name);
+                sub.AppendChild (val);
+                sub.AppendChild (val);
+                sub.AppendChild (val);
+                elementItems.AppendChild (sub);
+            }
+
+            elementHash.AppendChild (textHash);
+            element.AppendChild (elementHash);
+            elementType.AppendChild (textType);
+            element.AppendChild (elementType);
+            elementValue.AppendChild (textValue);
+            element.AppendChild (elementValue);
+
+            element.AppendChild (elementItems);
+
+            first.AppendChild (element);
+        }
+
+        public static void Serialize<T> (T root, Stream stm) where T : class {
+            var helper = new SerializationHelper ();
+            var dict = helper.SerializeFields (root);            
+            var rootHashCode = root.GetHashCode ();
+
+            XmlDocument doc = new XmlDocument ();
+            XmlDeclaration xmlDeclaration = doc.CreateXmlDeclaration ("1.0", "UTF-8", null);            
+            doc.InsertBefore (xmlDeclaration, null);
+
+            XmlElement first = doc.CreateElement ("", "first", "");
+            XmlText rootHash = doc.CreateTextNode (rootHashCode.ToString ());
+            first.AppendChild (rootHash);
+
+            foreach (var item in dict.Values) {
+                WriteToXml (doc, first, item);
+            }
+
+            doc.AppendChild (first);
+
+            doc.Save (stm);
+        }
+        public static T Deserialize<T> (Stream stm) where T : class {
+            XmlDocument doc = new XmlDocument ();
+            doc.Load (stm);
+            XmlNodeList nodelist = doc.SelectNodes ("first/item");
+
+            Dictionary<int, Item> itemCache = new Dictionary<int, Item> ();
+
+            foreach (XmlNode node in nodelist) {
+                Item item = new Item ();
+                item.HashCode = int.Parse (node.SelectSingleNode ("hashCode").FirstChild.Value);
+                item.Type = node.SelectSingleNode ("type").FirstChild.Value;
+                var value = node.SelectSingleNode ("value")?.FirstChild?.Value;
+                if (value != null) {
+                    item.Value = value;
+                }
+                var fields = node.SelectNodes ("items/item");
+                if (fields != null && fields.Count > 0) {
+                    item.Fields = new Dictionary<string, int> (fields.Count);
+
+                    foreach (XmlNode sub in fields) {
+                        var name = sub.SelectSingleNode ("name").FirstChild.Value;
+                        var fieldHashCode = int.Parse (sub.SelectSingleNode ("value").FirstChild.Value);
+                        item.Fields.Add (name, fieldHashCode);
+                    }
+                }
+
+                if (!itemCache.ContainsKey (item.HashCode))
+                    itemCache.Add (item.HashCode, item);
+            }
+
+            int rootHashCode = int.Parse (doc.SelectSingleNode ("first").FirstChild.Value);
+
+            DeserializationHelper helper = new DeserializationHelper (itemCache);
+            var result = helper.DeserializeFromCache (rootHashCode);
+
+            return (T) result;
+        }
+    }
+
+    class Program {      
+        static void Main (string[] args) {
+            var p1 = new Person { Name = "Homer", Age = 40 };
+            var p2 = new Person { Name = "Marge", Age = 30 };
+            p1.Spause = p2;
+            p2.Spause = p1;
+            using (var ms = new MemoryStream ()) {
+                Serializer.Serialize (p1, ms);
+                ms.Position = 0;
+                var p3 = Serializer.Deserialize<Person> (ms);
+                Debug.Assert (p3.Name == "Homer");
+                Debug.Assert (p3.Age == 40);
+                var p4 = p3.Spause;
+                Debug.Assert (p4.Spause == p3);
+                Debug.Assert (p4.Age == 30);
+            }
+        }
+    }
+}
